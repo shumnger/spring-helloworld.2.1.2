4cc5ee909096b775f4e4f0ec2e327d731d6b5221
Камиль Нагаев <shumnger@mail.ru>
2021-10-20 16:48:44 +0600

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean =  (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =  (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("Васька");
+        Cat cat1 = (Cat) applicationContext.getBean("Васька");
         System.out.println(bean.getMessage());
+
+        System.out.println(cat == cat1);
+        System.out.println(bean==bean1);
+
     }
 }
